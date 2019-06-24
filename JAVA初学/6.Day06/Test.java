8e0f560470cd73f01b4a1cd11b4cfdc3973f363b
shavekevin <shavekevin@gmail.com>
2019-06-24 20:34:28 +0800

    变量类型(待完善)

---
+++ JAVA初学/6.Day06/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+	public void pupAge(){
+		int age = 0;
+		age = age + 7;
+		System.out.println("小狗的年龄为:" + age);
+
+	}
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.pupAge();
+	}
+}
