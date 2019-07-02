3d286d1459b3cfb91669aa559f58b97361667ab4
shavekevin <shavekevin@gmail.com>
2019-07-02 23:41:36 +0800

    条件语句(已完成)

---
+++ JAVA初学/10.Day10/Test.java
@@ -0,0 +1,28 @@
+public  class Test {
+	public  static  void  main (String args[]) {
+		int x = 10;
+		if (x < 20){
+			system.out.println("满足if语句中的条件");
+		} else {
+			system.out.println("不满足if语句中的条件");
+
+		}
+		int y = 30;
+		if (y == 10) {
+			System.out.print("Value of y is 10");
+		} else if (y == 20) {
+			System.out.print("Value of y is 20");
+		} else if (y == 30) {
+			System.out.print("Value of y is 30");
+		} else {
+			System.out.print("这里是else语句");
+		}
+		int m = 30;
+		int n = 10;
+		if ( m == 30) {
+			if ( n == 10) {
+				System.out.print("这个语句满足m = 30, n = 10");
+			}
+		}
+	}
+}
