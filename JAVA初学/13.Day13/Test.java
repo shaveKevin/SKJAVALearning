e17ae3771208cdd96af5eecbfb9d72d27b98627d
shavekevin <shavekevin@gmail.com>
2019-07-05 15:13:05 +0800

    Character(已完成)

---
+++ JAVA初学/13.Day13/Test.java
@@ -0,0 +1,19 @@
+// char ch = 'a';
+// // Unicode 字符表示形式
+// char uniChar = '\u039A';
+// // 字符数组
+// char []charArray = {'a','b','c','d','e'}；
+
+// Character ch = new Character('a');
+
+// // 原始字符串'a'装箱到Character对象ch中
+// Character ch = 'a';
+// // 原始字符'x'用test
+// char c = test('x');
+
+public class Test {
+ 
+   public static void main(String args[]) {
+      System.out.println("开始\"Java学习!\"");
+   }
+}
