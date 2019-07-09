72da61705b8bb94b8493415ba0ec7b574d4df996
shavekevin <shavekevin@gmail.com>
2019-07-09 23:34:33 +0800

    StringBuffer StringBuilder(已完成)

---
+++ JAVA初学/15.Day15/StringDemo.java
@@ -0,0 +1,13 @@
+
+
+public class StringDemo {
+
+   public static void main(String args[]) {
+      StringBuffer sBuffer = new  StringBuffer("这是Java学习Demo:");
+      sBuffer.append("www");
+      sBuffer.append(".shavekevin");
+      sBuffer.append(".com");
+      System.out.println(sBuffer);
+
+   }
+}
