8b207610703d38eb6a4e2906145df02307486862
shavekevin <shavekevin@gmail.com>
2019-07-22 11:22:25 +0800

    接口(已完成)

--- JavaLearning/JAVA面向对象/6.Day06/InterfaceTest.java
+++ JavaLearning/JAVA面向对象/6.Day06/InterfaceTest.java
@@ -1,10 +1,24 @@
 public interface Sports {
-// 注意接口只负责声明不负责实现
+  // 注意接口只负责声明不负责实现
   public void setHomeTeam(String name);
+
   public void setVisitingTeam(String name);
 }
+
 public interface Football extends Sports {
   public void homeTeamSports(int points);
+
   public void visitingTeamScored(int points);
+
   public void endOfQuarter(int quarter);
+}
+
+public interface Hockey extends Sports {
+  public void homeGoalScored();
+
+  public void visitingGoalScored();
+
+  public void endOfPeriod(int period);
+
+  public void overtimePeriod(int ot);
 }
