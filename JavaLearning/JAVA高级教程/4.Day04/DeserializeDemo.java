8b8da96d2050e2727477573d3d29eaef781f0803
shavekevin <shavekevin@gmail.com>
2019-08-05 11:26:46 +0800

    网络编程（已完成）

--- JavaLearning/JAVA高级教程/4.Day04/DeserializeDemo.java
+++ JavaLearning/JAVA高级教程/4.Day04/DeserializeDemo.java
@@ -5,22 +5,19 @@ public class DeserializeDemo {
 	public static void main(String[] args) {
 
 		Employee e = null;
-		try
-		{
-		   FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
-		   ObjectInputStream in = new ObjectInputStream(fileIn);
-		   e = (Employee) in.readObject();
-		   in.close();
-		   fileIn.close();
-		}catch(IOException i)
-		{
-		   i.printStackTrace();
-		   return;
-		}catch(ClassNotFoundException c)
-		{
-		   System.out.println("Employee class not found");
-		   c.printStackTrace();
-		   return;
+		try {
+			FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			e = (Employee) in.readObject();
+			in.close();
+			fileIn.close();
+		} catch (IOException i) {
+			i.printStackTrace();
+			return;
+		} catch (ClassNotFoundException c) {
+			System.out.println("Employee class not found");
+			c.printStackTrace();
+			return;
 		}
 		System.out.println("Deserialized Employee...");
 		System.out.println("Name: " + e.name);
---
+++ JavaLearning/JAVA高级教程/5.Day05/URL处理.md
@@ -0,0 +1,5 @@
+
+## Java URL处理
+
+源地址：[Java URL处理](https://www.runoob.com/java/java-url-processing.html)
+github地址：[Java学习](https://github.com/shaveKevin/SKJAVALearning)
